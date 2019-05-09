48b950ff8b8dab0f6c1d448f85b64d47eca6aa3a
vidya ThandaSetty <vidyatshetty@gmail.com>
2019-05-09 11:52:13 -0500

    html changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
     address:       'smtp.gmail.com',
     port:          587,
     domain:         "gmail.com",
-    user_name:      "usertesting7433@gmail.com",
-    password:       "password", 
+    user_name:      "username@gmail.com",
+    password:       "password!", 
     authentication: :plain,
     enable_starttls_auto: true
   }
